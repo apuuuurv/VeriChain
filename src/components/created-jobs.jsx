4e2e0f48e3c54da388ce3f85d48d40e2513a778c
apuuuurv <apurvmadaye52@gmail.com>
2025-01-26 10:13:52 +0530

    Deployment

---
+++ src/components/created-jobs.jsx
@@ -0,0 +1,56 @@
+import { getApplications } from "@/api/apiApplications";
+import useFetch from "@/hooks/use-fetch";
+import { useUser } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import { BarLoader } from "react-spinners";
+import ApplicationCard from "./application-card";
+import { getMyJobs } from "@/api/apiJobs";
+import JobCard from "./job-card";
+
+const CreatedJobs = () => {
+  const { user } = useUser();
+
+  const {
+    loading: loadingCreatedJobs,
+    data: createdJobs,
+    fn: fnCreatedJobs,
+  } = useFetch(getMyJobs, {
+    recruiter_id: user.id,
+  });
+
+  useEffect(() => {
+    console.log("User  ID:", user.id);
+    fnCreatedJobs();
+  }, [user.id]);
+
+  if (loadingCreatedJobs) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+
+  return (
+    <div>
+      {loadingCreatedJobs ? (
+        <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
+      ) : (
+        <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {createdJobs?.length ? (
+            createdJobs.map((job) => {
+              return (
+                <JobCard
+                  key={job.id}
+                  job={job}
+                  onJobSaved={fnCreatedJobs}
+                  isMyJob
+                />
+              );
+            })
+          ) : (
+            <div>No Jobs Found 😢</div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CreatedJobs;
